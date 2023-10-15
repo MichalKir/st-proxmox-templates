7079556f7c1bf3e53e3c35eba884fc2fa9b30dc5
Michal Kirejczyk <michalkirejczyk@outlook.com>
2023-10-15 11:24:27 +0200

    unmounts virtio iso and changes to bits for download

--- windows/setup-server.ps1
+++ windows/setup-server.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = "Stop"
 # WinRM
 Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 Get-NetFirewallRule -DisplayGroup "Windows Remote Management" | Enable-NetFirewallRule
+Set-WSManQuickConfig -Force
 Enable-PSRemoting -Force -Confirm:$false
 Set-WSManInstance -ResourceURI winrm/config/client/auth -ValueSet @{Basic="true"} 
 Set-WSManInstance -ResourceURI winrm/config/service/auth -ValueSet @{Basic="true"} 
@@ -9,11 +10,14 @@ Set-WSManInstance -ResourceURI winrm/config/service -ValueSet @{AllowUnencrypted
 
 # Update VirtIO Drivers
 $virtioIsoDriversPath = Join-Path -Path $env:temp -ChildPath "virtio-win.iso"
-Invoke-WebRequest -Uri "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso" -OutFile $virtioDriversPath
-$virtioIsoMountPath = (Mount-DiskImage -ImagePath $virtioIsoDriversPath -StorageType ISO | Get-Volume).DriveLetter, ":" -join ""
-Get-ChildItem -Path $virtioIsoMountPath  -Include "qemu-ga-x86_64.msi", "virtio-win-gt-x64.msi" -Recurse | ForEach-Object {
+Start-BitsTransfer -Source "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso" -Description $virtioIsoDriversPath
+
+$virtioIsoMount = Mount-DiskImage -ImagePath $virtioIsoDriversPath -StorageType ISO
+$virtioIsoMountDrive = ($virtioIsoMount | Get-Volume).DriveLetter, ":" -join ""
+Get-ChildItem -Path $virtioIsoMountDrive -Include "qemu-ga-x86_64.msi", "virtio-win-gt-x64.msi" -Recurse | ForEach-Object {
     Start-Process -FilePath msiexec -ArgumentList "/i `"$($_.FullName)`" /qn /norestart ADDLOCAL=ALL" -Wait -PassThru -NoNewWindow
 }
+$virtioIsoMount | Dismount-DiskImage
 Start-Service -Name QEMU-GA
 
 # Reset Winlogon Count
