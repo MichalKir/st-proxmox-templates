96c061cc988e4b2dbb2574359a52c1fe771ea383
Michal Kirejczyk <michalkirejczyk@outlook.com>
2023-10-15 10:30:26 +0200

    adds setup windows server script

---
+++ windows/setup-server.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = "Stop"
+# WinRM
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
+Get-NetFirewallRule -DisplayGroup "Windows Remote Management" | Enable-NetFirewallRule
+Enable-PSRemoting -Force -Confirm:$false
+
+# Update VirtIO Drivers
+$virtioIsoDriversPath = Join-Path -Path $env:temp -ChildPath "virtio-win.iso"
+Invoke-WebRequest -Uri "https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso" -OutFile $virtioDriversPath
+$virtioIsoMountPath = (Mount-DiskImage -ImagePath $virtioIsoDriversPath -StorageType ISO | Get-Volume).DriveLetter, ":" -join ""
+Get-ChildItem -Path $virtioIsoMountPath  -Include "qemu-ga-x86_64.msi", "virtio-win-gt-x64.msi" -Recurse | ForEach-Object {
+    Start-Process -FilePath msiexec -ArgumentList "/i `"$($_.FullName)`" /qn /norestart ADDLOCAL=ALL" -Wait -PassThru -NoNewWindow
+}
+Start-Service -Name QEMU-GA
+
+# Reset Winlogon Count
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
